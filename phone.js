8159fe3a18380ffe1cdd154d1c89eae396b8926b
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-03-01 23:17:40 +0600

    commit

--- phone.js
+++ phone.js
@@ -1,8 +1,32 @@
-function searchPhone=()=>{
+const searchPhone = ()=>{
     const searchfield = document.getElementById('search-field');
     const searchText = searchfield.value;
+    // console.log(searchText);
+    searchText.textContent = '';
 
-    const url = 
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data));
 
+}
 
+const displaySearchResult = phones =>{
+    const searchResult = document.getElementById('search-result');
+    for(const phone of phones){
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML= `
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      `;
+      searchResult.appendChild(div);
+    } 
 }
