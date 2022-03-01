9cb74e5aafa10bc6fe367b1e2176d2c7a624ddeb
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-03-02 01:17:41 +0600

    new commit

--- phone.js
+++ phone.js
@@ -22,7 +22,6 @@ const displaySearchResult = phones =>{
           <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
           <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
-            <h5>${phone.slug}</h5>
             <p class="card-text">${phone.phone_name} has a 6.7-inch1 all-screen Super Retina XDR display with ProMotion. The back is textured matte glass, and there's a flat-edge stainless steel band around the frame. </p>
           </div>
         </div>
@@ -32,8 +31,8 @@ const displaySearchResult = phones =>{
     }) 
 }
 
-const loadPhoneDetails =()=>{
-    const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`;
+const loadPhoneDetails =(phoneID)=>{
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneID}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhoneDetails(data));
@@ -43,16 +42,19 @@ const loadPhoneDetails =()=>{
 const displayPhoneDetails = phone =>{
     console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent='';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML=`
-    <img src="..." class="card-img-top" alt="...">
+    <img src="${phone.image}" class="card-img-top w-50" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <h5>${phone.slug}</h5>
+              <p class="card-text">${phone.phone_name} has a 6.7-inch1 all-screen Super Retina XDR display with ProMotion. The back is textured matte glass, and there's a flat-edge stainless steel band around the frame.</p>
+              <a href="#" class="btn btn-primary">Details</a>
             </div>
     `;
     phoneDetails.appendChild(div);
+    
 }
 
