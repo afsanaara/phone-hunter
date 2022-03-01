49f07b903a775bd378e41077d1d2f3be10e000b8
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-03-02 00:42:34 +0600

    new commit

--- phone.js
+++ phone.js
@@ -18,7 +18,7 @@ const displaySearchResult = phones =>{
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML= `
-        <div onclick="" class="card">
+        <div onclick="loadPhoneDetails()" class="card">
           <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
           <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
@@ -33,5 +33,26 @@ const displaySearchResult = phones =>{
 }
 
 const loadPhoneDetails =()=>{
-    
+    const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetails(data));
+
 }
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML=`
+    <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `;
+    phoneDetails.appendChild(div);
+}
+
